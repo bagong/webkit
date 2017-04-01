bagong/webkit

commit 0f50103f775c628c20c93c86e5bb9236760c72aa

[EFL][WK2] Remove duplicated code to set size to the DrawingArea. https://bugs.webkit.org/show_bug.cgi?id=131972
Reviewed by Andreas Kling.

DrawingArea's size is set in the WebView.cpp when EwkView's size is
changed, so we don't have to set again in the PageViewportController.

* UIProcess/CoordinatedGraphics/PageViewportController.cpp:
(WebKit::PageViewportController::didChangeViewportSize):


Canonical link: https://commits.webkit.org/150082@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@167665 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/UIProcess/CoordinatedGraphics/PageViewportController.cpp
@@ -246,10 +246,6 @@ void PageViewportController::didChangeViewportSize(const FloatSize& newSize)
         return;
 
     m_viewportSize = newSize;
-
-    // Let the WebProcess know about the new viewport size, so that
-    // it can resize the content accordingly.
-    m_webPageProxy->drawingArea()->setSize(roundedIntSize(newSize), IntSize(), IntSize());
 }
 
 void PageViewportController::didChangeContentsVisibility(const FloatPoint& position, float scale, const FloatPoint& trajectoryVector)

commit 5f00ee40eea6675092abafac1c27cb7eab93fe42

Let WebProcess know correct viewport size
Behavior was broken in r167665.

Change-Id: Ic40d56c78d4c8f46c8503fc1c489e79f498b2eb5

Source/WebKit2/UIProcess/CoordinatedGraphics/PageViewportController.cpp
@@ -249,6 +249,12 @@ void PageViewportController::didChangeViewportSize(const FloatSize& newSize)
         return;
 
     m_viewportSize = newSize;
+
+#if PLATFORM(QT)
+    // Let the WebProcess know about the new viewport size, so that
+    // it can resize the content accordingly.
+    m_webPageProxy->drawingArea()->setSize(roundedIntSize(newSize), IntSize(), IntSize());
+#endif
 }
 
 void PageViewportController::didChangeContentsVisibility(const FloatPoint& position, float scale, const FloatPoint& trajectoryVector)